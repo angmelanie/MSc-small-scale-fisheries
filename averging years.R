35d21f2ec66f2f52b2b82a5df48f98975e9490ad
Melanie <melanieang07@gmail.com>
2017-10-04 10:27:23 -0700

    Averaging years for all EEZs (Alaska, Canada, USA, Mexico)

--- averging years.R
+++ averging years.R
@@ -8,13 +8,107 @@
 # I've also removed all the error species from the folder
 # You need to do this for each RCP scenario and for each EEZ
 
+# ALASKA - 2.6 and 8.5
 
-path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL26/"
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL85/"
 
 filename <- dir(path, pattern = ".csv")
-filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL26/", filename, sep = "")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL85/", filename, sep = "")
 
-for (i in 1:2){
+for (i in 1:length(filepath)){
+  sau_dbem <- read.csv(filepath[i], header = TRUE)
+  
+  # remove rows with duplicated cell IDs -  error from 7final.R
+  sau_dbem <- sau_dbem[!duplicated(sau_dbem$INDEX),]
+  
+  # prepare data frame for analysis
+  flip_sau_dbem <- data.frame(t(sau_dbem))
+  
+  # average every 20 years
+  smoothMean <- rollmean(x = flip_sau_dbem, # original series 
+                         k = 20) # width of the rolling window 
+  # tidying data
+  colnames(smoothMean) <- flip_sau_dbem[1,]
+  smoothMean <- smoothMean[-1,]
+  rownames(smoothMean) <- c(2000:2051)
+  avg_sau_dbem <- data.frame(t(smoothMean))
+  
+  # final result
+  newfilepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL85/", filename[i], "_avg.csv", sep = "")
+  
+  write.csv(avg_sau_dbem, newfilepath)
+}
+
+# CANADA - 2.6 and 8.5
+
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL85/"
+
+filename <- dir(path, pattern = ".csv")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL85/", filename, sep = "")
+
+for (i in 1:length(filepath)){
+  sau_dbem <- read.csv(filepath[i], header = TRUE)
+  
+  # remove rows with duplicated cell IDs -  error from 7final.R
+  sau_dbem <- sau_dbem[!duplicated(sau_dbem$INDEX),]
+  
+  # prepare data frame for analysis
+  flip_sau_dbem <- data.frame(t(sau_dbem))
+  
+  # average every 20 years
+  smoothMean <- rollmean(x = flip_sau_dbem, # original series 
+                         k = 20) # width of the rolling window 
+  # tidying data
+  colnames(smoothMean) <- flip_sau_dbem[1,]
+  smoothMean <- smoothMean[-1,]
+  rownames(smoothMean) <- c(2000:2051)
+  avg_sau_dbem <- data.frame(t(smoothMean))
+  
+  # final result
+  newfilepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL85/", filename[i], "_avg.csv", sep = "")
+  
+  write.csv(avg_sau_dbem, newfilepath)
+}
+
+# USA - 2.6 and 8.5
+
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL85/"
+
+filename <- dir(path, pattern = ".csv")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL85/", filename, sep = "")
+
+for (i in 1:length(filepath)){
+  sau_dbem <- read.csv(filepath[i], header = TRUE)
+  
+  # remove rows with duplicated cell IDs -  error from 7final.R
+  sau_dbem <- sau_dbem[!duplicated(sau_dbem$INDEX),]
+  
+  # prepare data frame for analysis
+  flip_sau_dbem <- data.frame(t(sau_dbem))
+  
+  # average every 20 years
+  smoothMean <- rollmean(x = flip_sau_dbem, # original series 
+                         k = 20) # width of the rolling window 
+  # tidying data
+  colnames(smoothMean) <- flip_sau_dbem[1,]
+  smoothMean <- smoothMean[-1,]
+  rownames(smoothMean) <- c(2000:2051)
+  avg_sau_dbem <- data.frame(t(smoothMean))
+  
+  # final result
+  newfilepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL85/", filename[i], "_avg.csv", sep = "")
+  
+  write.csv(avg_sau_dbem, newfilepath)
+}
+
+# Mexico - 2.6 and 8.5
+
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Mexico/GFDL26/"
+
+filename <- dir(path, pattern = ".csv")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Mexico/GFDL26/", filename, sep = "")
+
+for (i in 1:length(filepath)){
   sau_dbem <- read.csv(filepath[i], header = TRUE)
   
   # remove rows with duplicated cell IDs -  error from 7final.R
@@ -37,3 +131,4 @@ for (i in 1:2){
   
   write.csv(avg_sau_dbem, newfilepath)
 }
+
