0f678a5b9714fb2b07ed161c0494653037064d95
Melanie <melanieang07@gmail.com>
2017-10-11 12:05:23 -0700

    update catch by demersal-pelagic

--- catch-by-DemersPelag26.R
+++ catch-by-DemersPelag26.R
@@ -1,53 +1,9 @@
-# DEMERSAL-PELAGIC
-# How will SSF catches of demersal-pelagic species be impacted by CC?
-
-# import taxa list we have
-taxalist <- read.csv("C:/Users/angmel/Documents/firstchapter/ANALYSIS/taxa_cygwin_DROBO.csv")
-# import cygwin known demerspelag info
-cygwin_dp <- read.csv("C:/Users/angmel/Documents/firstchapter/ANALYSIS/taxa_cygwin_demerpelag.csv") %>% 
-  rename(TaxonID = TaxonKey)
-
-# join above species list with my access list (species i have demerspelagic info for)
-taxalist_dp <- full_join(taxalist, cygwin_dp, by = "TaxonID") # incomplete, building
-
-# remainder list, find sci name via join
-drobo_taxaID <- taxalist_dp %>% 
-  filter(Source %in% "DROBO") %>% 
-  dplyr::select(TaxonID)
-
-sci_common <- read.csv("C:/Users/angmel/Documents/firstchapter/Reference tables/taxonID.csv") %>% 
-  rename(TaxonID = taxonID)
-
-drobo_dp_sci_ID <- right_join(sci_common, drobo_taxaID, by = "TaxonID") 
-drobo_dp_sci <- drobo_dp_sci_ID %>% 
-  dplyr::select(taxon_name)
-
-# run this through rfishbase demerspelag
-library(rfishbase)
-
-drobo_dp <- species(drobo_dp_sci$taxon_name, fields = "DemersPelag") %>% 
-  dplyr::select(sciname, DemersPelag)
-
-unique(drobo_dp$DemersPelag) # find variations
-
-# rfishbase: not just Demer/Pela/Reef but other combination, simplifying to 3 options
-DemersPelag <- c("pelagic-neritic", "pelagic-oceanic", "demersal", "reef-associated", "benthopelagic", "bathydemersal")
-DemersPelag_2 <- c("Pelagic", "Pelagic", "Demersal", "reef-associated", "Pelagic", "Demersal")
-lookup <- data.frame(DemersPelag, DemersPelag_2)
-
-# form lookup table of speciesID, demerspelag and eez
-taxa_demerspelag <- left_join(drobo_dp, lookup, by = "DemersPelag") %>%   # lookup to 1/3 options
-  dplyr::select(-(DemersPelag)) %>%                   # remove the variations
-  rename(taxon_name = sciname) %>% 
-  full_join(drobo_dp_sci_ID) %>% 
-  rename(DemersPelag = DemersPelag_2)
-
-# let's work with RCP 85 - import list of species from each EEZ, keep eez as a column
+# let's work with RCP 26 - import list of species from each EEZ, keep eez as a column
 # -----
 # CANADA
-path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL85/avg/"
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL26/avg/"
 filename <- dir(path)
-filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL85/avg/", filename, sep = "")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Canada/GFDL26/avg/", filename, sep = "")
 
 # AGGREGATE TOTAL - FOR LOOP
 # create empty list for aggregation
@@ -70,7 +26,7 @@ for (i in 1:length(filepath)){
     summarize(year_total = sum(catch)) %>% 
     data.frame()
   p$taxaID <- substr(filename[i],1,6) #extract from path the taxa ID & assign as column
-
+  
   datalist[[i]] <- p # attaches each csv file as a new list
 }
 
@@ -79,9 +35,9 @@ Canada_dp <- dplyr::bind_rows(datalist)
 Canada_dp$country <- "Canada"
 
 # ALASKA
-path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL85/avg/"
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL26/avg/"
 filename <- dir(path)
-filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL85/avg/", filename, sep = "")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Alaska/GFDL26/avg/", filename, sep = "")
 
 # AGGREGATE TOTAL - FOR LOOP
 # create empty list for aggregation
@@ -114,9 +70,9 @@ Alaska_dp$country <- "Alaska"
 
 
 # UNITED STATES
-path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL85/avg/"
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL26/avg/"
 filename <- dir(path)
-filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL85/avg/", filename, sep = "")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/USA/GFDL26/avg/", filename, sep = "")
 
 # AGGREGATE TOTAL - FOR LOOP
 # create empty list for aggregation
@@ -148,9 +104,9 @@ USA_dp <- dplyr::bind_rows(datalist)
 USA_dp$country <- "USA"
 
 # MEXICO
-path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Mexico/GFDL85/avg/"
+path <- "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Mexico/GFDL26/avg/"
 filename <- dir(path)
-filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Mexico/GFDL85/avg/", filename, sep = "")
+filepath <- paste("C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/Mexico/GFDL26/avg/", filename, sep = "")
 
 # AGGREGATE TOTAL - FOR LOOP
 # create empty list for aggregation
@@ -182,37 +138,40 @@ Mexico_dp <- dplyr::bind_rows(datalist)
 Mexico_dp$country <- "Mexico"
 
 # BIND ALL EEZS
-RCP85_dp <- dplyr::bind_rows(Alaska_dp, Canada_dp) %>% 
+RCP26_dp <- dplyr::bind_rows(Alaska_dp, Canada_dp) %>% 
   dplyr::bind_rows(USA_dp) %>% 
   dplyr::bind_rows(Mexico_dp) %>% 
   rename(TaxonID = taxaID) # making this compatible with demerspelag table
-RCP85_dp$TaxonID <- as.numeric(RCP85_dp$TaxonID) # likewise making compatible
+RCP26_dp$TaxonID <- as.numeric(RCP26_dp$TaxonID) # likewise making compatible
 
-class(RCP85_dp$TaxonID)
+class(RCP26_dp$TaxonID)
 
 # link to demersal-pelagic sheet ----
-RCP85 <- left_join(RCP85_dp, taxa_demerspelag, by = "TaxonID")
+RCP26 <- left_join(RCP26_dp, taxa_demerspelag, by = "TaxonID")
 
-RCP85d <- RCP85 %>% 
+RCP26d <- RCP26 %>% 
   filter(DemersPelag == "Demersal") %>% 
   group_by(year) %>% 
   summarize(year_t = sum(year_total))
-RCP85d
+RCP26d
 
-RCP85p <- RCP85 %>% 
+RCP26p <- RCP26 %>% 
   filter(DemersPelag == "Pelagic") %>% 
   group_by(year) %>% 
   summarize(year_t = sum(year_total))
 
-RCP85reef <- RCP85 %>% 
+RCP26reef <- RCP26 %>% 
   filter(DemersPelag == "reef-associated") %>% 
   group_by(year) %>% 
   summarize(year_t = sum(year_total))
 
 # plot it:
-test <- RCP85 %>% 
+test26 <- RCP26 %>% 
   group_by(DemersPelag, year) %>% 
   summarize(sum(year_total)) %>% 
   ggplot(aes(x=year, y = `sum(year_total)`)) +
   geom_point(aes(colour = DemersPelag)) +
   theme_bw()
+
+ggsave("catch-by-demerpel-26.png", path = "C:/Users/angmel/Documents/firstchapter/SAU_DBEM_RESULTS/AGGREGATE/")
+
